502f0396eea1a1673818e644a60f0fc2cce9c8e1
Dr. Off <70072513+dr-off@users.noreply.github.com>
2021-02-06 23:12:36 -0500

    New populateArrayFromListElement util function.

--- modules/util.js
+++ modules/util.js
@@ -1,12 +1,49 @@
 //
+// Dependencies
+//
+
+const cheerio = require("cheerio"); // For JSDoc Comments
+
+//
 // Exports
 //
 
 const util = {};
 
+/**
+ * Removes commas from a string and tries to parse an int out of it.
+ * 
+ * @param {String} text A string of a number like "100" or "1,234,567".
+ */
 util.cleanAndParseInt = function(text)
 {
 	return parseInt(text.trim().replace(",", ""));
+}
+
+/**
+ * Populates an array from a list element selected with the given selector.
+ * 
+ * @param {cheerio.Root} $ A cheerio Root instance.
+ * @param {Array} listArray The array to populate from the list element.
+ * @param {String} listElementSelector The selector for the list element.
+ * @param {Object} indexLookupObject 
+ */
+util.populateArrayFromListElement = function($, listArray, listElementSelector, indexLookupObject)
+{
+	$(listElementSelector).each(function(i, element)
+	{
+		let text = $(this).text();
+
+		let entry = {};
+
+		if(indexLookupObject != undefined)
+			entry.index = indexLookupObject[text];
+
+		entry.name = text;
+		entry.url = $(this).children("a").prop("href");
+
+		listArray.push(entry);
+	});
 }
 
 module.exports = util;
